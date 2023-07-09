b358f015723430ea84b2c122b83257d2d5d95a58
Vladimir Kharchenko <vombat007@gmail.com>
2023-07-09 14:43:28 +0300

    update React FlightDetails.js

--- DjangoAir_ERP_system/frontend/src/components/FlightDetails.js
+++ DjangoAir_ERP_system/frontend/src/components/FlightDetails.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import {Button, Form} from "react-bootstrap";
+import {Button, Form} from 'react-bootstrap';
 
 const FlightDetails = ({flightId, handleBack}) => {
     const [seatTypes, setSeatTypes] = useState([]);
     const [options, setOptions] = useState([]);
+    const [selectedSeatType, setSelectedSeatType] = useState('');
+    const [selectedOptions, setSelectedOptions] = useState([]);
 
     useEffect(() => {
         axios
@@ -22,27 +24,73 @@ const FlightDetails = ({flightId, handleBack}) => {
             });
     }, [flightId]);
 
+    const handleSeatTypeChange = (event) => {
+        const selectedType = event.target.value;
+        const seatType = seatTypes.find((type) => type.seat_type === selectedType);
+        setSelectedSeatType(seatType);
+    };
+
+    const handleOptionChange = (event) => {
+        const selectedOptionId = parseInt(event.target.value);
+        const option = options.find((opt) => opt.id === selectedOptionId);
+
+        if (event.target.checked) {
+            setSelectedOptions((prevOptions) => [...prevOptions, option]);
+        } else {
+            setSelectedOptions((prevOptions) =>
+                prevOptions.filter((opt) => opt.id !== selectedOptionId)
+            );
+        }
+    };
+
+    const calculateTotalPrice = () => {
+        let totalPrice = selectedSeatType ? selectedSeatType.price : 0;
+
+        for (const option of selectedOptions) {
+            totalPrice += option.price;
+        }
+
+        return totalPrice;
+    };
+
     return (
         <div>
             <h2>Seat Type Options</h2>
             <p>Flight ID: {flightId}</p>
-            {/* Render the seat types and options */}
+
+            {/* Render the seat types */}
             <h3>Seat Types</h3>
-            <ul>
+            <Form>
                 {seatTypes.map((seatType) => (
-                    <li key={seatType.id}> {seatType.seat_type} Price {seatType.price} Quantity of seats {seatType.quantity}</li>
+                    <Form.Check
+                        key={seatType.id}
+                        type="radio"
+                        label={`${seatType.seat_type} Price ${seatType.price} Quantity of seats ${seatType.quantity}`}
+                        value={seatType.seat_type}
+                        name="seatType"
+                        checked={selectedSeatType && selectedSeatType.id === seatType.id}
+                        onChange={handleSeatTypeChange}
+                    />
                 ))}
-            </ul>
-            <h3>Options</h3>
-            <ul>
+                <h3>Options</h3>
                 {options.map((option) => (
-                    <li key={option.id}>{option.name} Price {option.price}</li>
+                    <Form.Check
+                        key={option.id}
+                        type="checkbox"
+                        label={`${option.name} Price ${option.price}`}
+                        value={option.id}
+                        checked={selectedOptions.some((opt) => opt.id === option.id)}
+                        onChange={handleOptionChange}
+                    />
                 ))}
-            </ul>
+            </Form>
+
+            {/* Display the total price */}
+            <p>Total Price: {calculateTotalPrice()}</p>
+
             <Button variant="secondary" onClick={handleBack}>
                 Back
             </Button>
-
         </div>
     );
 };
