b6a23c39297b3f3dcdaa07b7869127bf2ee397a5
Vladimir Kharchenko <vombat007@gmail.com>
2023-07-09 13:50:56 +0300

    update React FlightDetails.js and Flight.js

--- DjangoAir_ERP_system/frontend/src/components/Flight.js
+++ DjangoAir_ERP_system/frontend/src/components/Flight.js
@@ -1,110 +1,119 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import DatePicker from 'react-datepicker';
-import { createRoot } from 'react-dom/client';
-import { Form, Button } from 'react-bootstrap';
-
-import SeatTypeOptions from './FlightDetails'; // Import the new component
+import {createRoot} from 'react-dom/client';
+import {Form, Button} from 'react-bootstrap';
+import FlightDetails from './FlightDetails';
 
 const FlightsSearch = () => {
-  const [destination, setDestination] = useState('');
-  const [departureDate, setDepartureDate] = useState(new Date());
-  const [seatCount, setSeatCount] = useState('');
-  const [flights, setFlights] = useState([]);
-  const [showNoFlightsMessage, setShowNoFlightsMessage] = useState(false);
-  const [selectedFlightId, setSelectedFlightId] = useState(null); // Track the selected flight ID
-  const [showSeatTypeOptions, setShowSeatTypeOptions] = useState(false); // Flag to show/hide seat type options
+    const [destination, setDestination] = useState('');
+    const [departureDate, setDepartureDate] = useState(new Date());
+    const [seatCount, setSeatCount] = useState('');
+    const [flights, setFlights] = useState([]);
+    const [showNoFlightsMessage, setShowNoFlightsMessage] = useState(false);
+    const [selectedFlightId, setSelectedFlightId] = useState(null); // Track the selected flight ID
+    const [showSeatTypeOptions, setShowSeatTypeOptions] = useState(false); // Flag to show/hide seat type options
 
-  const handleSearch = () => {
-    if (!destination || Number(destination)) {
-      alert('Please enter a valid destination');
-      return;
-    }
+    const handleSearch = () => {
+        if (!destination || Number(destination)) {
+            alert('Please enter a valid destination');
+            return;
+        }
 
-    if (!seatCount || isNaN(Number(seatCount)) || Number(seatCount) < 0) {
-      alert('Please enter a valid seat count.');
-      return;
-    }
+        if (!seatCount || isNaN(Number(seatCount)) || Number(seatCount) < 0) {
+            alert('Please enter a valid seat count.');
+            return;
+        }
 
-    axios
-      .get('/api/flight/search/', {
-        params: {
-          destination: destination,
-          departure_date: departureDate.toISOString().split('T')[0], // Format date to 'YYYY-MM-DD'
-          seats_count: seatCount,
-        },
-      })
-      .then((response) => {
-        setFlights(response.data);
-        setShowNoFlightsMessage(response.data.length === 0); // Set the flag based on the response
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+        axios
+            .get('/api/flight/search/', {
+                params: {
+                    destination: destination,
+                    departure_date: departureDate.toISOString().split('T')[0], // Format date to 'YYYY-MM-DD'
+                    seats_count: seatCount,
+                },
+            })
+            .then((response) => {
+                setFlights(response.data);
+                setShowNoFlightsMessage(response.data.length === 0); // Set the flag based on the response
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
 
-  const handleNextStep = (flightId) => {
-    // Set the selected flight ID and show seat type options
-    setSelectedFlightId(flightId);
-    setShowSeatTypeOptions(true);
-  };
+    const handleNextStep = (flightId) => {
+        // Set the selected flight ID and show seat type options
+        setSelectedFlightId(flightId);
+        setShowSeatTypeOptions(true);
+    };
 
-  return (
-    <div>
-      <h1>Flight Search</h1>
-      <Form.Group controlId="destination">
-        <Form.Control
-          type="text"
-          placeholder="Destination"
-          value={destination}
-          onChange={(e) => setDestination(e.target.value)}
-        />
-      </Form.Group>
-      <Form.Group controlId="seatCount">
-        <Form.Label>Seat Count</Form.Label>
-        <Form.Control
-          type="number"
-          placeholder="Seat Count"
-          value={seatCount}
-          onChange={(e) => setSeatCount(e.target.value)}
-        />
-      </Form.Group>
-      <Form.Group controlId="departureDate">
-        <Form.Label>Departure Date</Form.Label>
-        <br />
-        <DatePicker
-          selected={departureDate}
-          onChange={(date) => setDepartureDate(date)}
-          dateFormat="yyyy-MM-dd"
-          className="form-control"
-          placeholderText="Departure Date (YY-MM-DD)"
-        />
-      </Form.Group>
-      <Button variant="primary" onClick={handleSearch}>
-        Search
-      </Button>
-      {showNoFlightsMessage ? (
-        <p>No flights available for the given criteria. Please choose a different date or destination.</p>
-      ) : (
+    const handleBack = () => {
+        // Reset the selected flight ID and hide seat type options
+        setSelectedFlightId(null);
+        setShowSeatTypeOptions(false);
+    };
+
+    return (
         <div>
-          {flights.map((flight) => (
-            <div key={flight.flight_id}>
-              <p>Destination: {flight.destination}</p>
-              <p>Departure Date: {flight.departure_date}</p>
-              <Button variant="info" onClick={() => handleNextStep(flight.flight_id)}>
-                Next Step
-              </Button>
-            </div>
-          ))}
+            {showSeatTypeOptions ? (
+                <FlightDetails flightId={selectedFlightId} handleBack={handleBack}/>
+            ) : (
+                <div>
+                    <h1>Flight Search</h1>
+                    <Form.Group controlId="destination">
+                        <Form.Control
+                            type="text"
+                            placeholder="Destination"
+                            value={destination}
+                            onChange={(e) => setDestination(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="seatCount">
+                        <Form.Label>Seat Count</Form.Label>
+                        <Form.Control
+                            type="number"
+                            placeholder="Seat Count"
+                            value={seatCount}
+                            onChange={(e) => setSeatCount(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="departureDate">
+                        <Form.Label>Departure Date</Form.Label>
+                        <br/>
+                        <DatePicker
+                            selected={departureDate}
+                            onChange={(date) => setDepartureDate(date)}
+                            dateFormat="yyyy-MM-dd"
+                            className="form-control"
+                            placeholderText="Departure Date (YY-MM-DD)"
+                        />
+                    </Form.Group>
+                    <Button variant="primary" onClick={handleSearch}>
+                        Search
+                    </Button>
+                    {showNoFlightsMessage ? (
+                        <p>No flights available for the given criteria. Please choose a different date or
+                            destination.</p>
+                    ) : (
+                        <div>
+                            {flights.map((flight) => (
+                                <div key={flight.flight_id}>
+                                    <p>Destination: {flight.destination}</p>
+                                    <p>Departure Date: {flight.departure_date}</p>
+                                    <Button variant="info" onClick={() => handleNextStep(flight.flight_id)}>
+                                        Next Step
+                                    </Button>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
+            )}
         </div>
-      )}
-      {showSeatTypeOptions && (
-        <SeatTypeOptions flightId={selectedFlightId} setShowSeatTypeOptions={setShowSeatTypeOptions} />
-      )}
-    </div>
-  );
+    );
 };
 
 const container = document.getElementById('app');
 const root = createRoot(container);
-root.render(<FlightsSearch />);
+root.render(<FlightsSearch/>);
--- DjangoAir_ERP_system/frontend/src/components/FlightDetails.js
+++ DjangoAir_ERP_system/frontend/src/components/FlightDetails.js
@@ -1,50 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-const FlightDetails = ({ flightId, setShowSeatTypeOptions }) => {
-  const [seatTypes, setSeatTypes] = useState([]);
-  const [options, setOptions] = useState([]);
+const FlightDetails = ({flightId, handleBack}) => {
+    const [seatTypes, setSeatTypes] = useState([]);
+    const [options, setOptions] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get('/api/flight/options/', {
-        params: {
-          flight_id: flightId,
-        },
-      })
-      .then((response) => {
-        setSeatTypes(response.data.seat_types);
-        setOptions(response.data.options);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, [flightId]);
+    useEffect(() => {
+        axios
+            .get('/api/flight/options/', {
+                params: {
+                    flight_id: flightId,
+                },
+            })
+            .then((response) => {
+                setSeatTypes(response.data.seat_types);
+                setOptions(response.data.options);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, [flightId]);
 
-  const handleBack = () => {
-    setShowSeatTypeOptions(false); // Hide seat type options
-  };
+    return (
+        <div>
+            <h2>Seat Type Options</h2>
+            <p>Flight ID: {flightId}</p>
+            {/* Render the seat types and options */}
+            <h3>Seat Types</h3>
+            <ul>
+                {seatTypes.map((seatType) => (
+                    <li key={seatType.id}>{seatType.seat_type} Quantity of seats {seatType.quantity}</li>
+                ))}
+            </ul>
+            <h3>Options</h3>
+            <ul>
+                {options.map((option) => (
+                    <li key={option.id}>{option.name} Price {option.price}</li>
+                ))}
+            </ul>
+            <button onClick={handleBack}>Back</button>
 
-  return (
-    <div>
-      <h2>Seat Type Options</h2>
-      <p>Flight ID: {flightId}</p>
-      <button type="button" class="btn btn-secondary"  onClick={handleBack}>Back</button>
-      {/* Render the seat types and options */}
-      <h3>Seat Types</h3>
-      <ul>
-        {seatTypes.map((seatType) => (
-          <li key={seatType.id}>{seatType.seat_type} Quantity of seats {seatType.quantity}</li>
-        ))}
-      </ul>
-      <h3>Options</h3>
-      <ul>
-        {options.map((option) => (
-          <li key={option.id}>{option.name} Price {option.price}</li>
-        ))}
-      </ul>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default FlightDetails;
