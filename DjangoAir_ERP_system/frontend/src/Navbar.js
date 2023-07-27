28234cdfa024dd7071abf898f9f8823db0773810
Vladimir Kharchenko <vombat007@gmail.com>
2023-07-27 21:01:02 +0300

    update Navbar and remove div "app" from index.html

--- DjangoAir_ERP_system/frontend/src/Navbar.js
+++ DjangoAir_ERP_system/frontend/src/Navbar.js
@@ -1,57 +1,69 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
+import React, {useState} from 'react';
 import Registrate from './components/Registrate';
 import Login from './components/Login';
 import CustomerCabinetView from "./components/Customer_cabinet"
 import FlightsSearch from "./components/Flight";
 
+
 const Navbar = () => {
+    const [activeComponent, setActiveComponent] = useState(null);
+
     const handleCabinetClick = () => {
-        // Render the CustomerCabinetView component when the "Cabinet" button is clicked
-        const container = document.getElementById('app');
-        const root = createRoot(container);
-        root.render(<CustomerCabinetView/>);
+        setActiveComponent('Cabinet');
     };
 
     const handleFlightSearchClick = () => {
-        const container = document.getElementById('app');
-        const root = createRoot(container);
-        root.render(<FlightsSearch/>)
-
+        setActiveComponent('FlightSearch');
     }
 
+    // Helper function to render the appropriate component based on activeComponent
+    const renderActiveComponent = () => {
+        switch (activeComponent) {
+            case 'Cabinet':
+                return <CustomerCabinetView/>;
+            case 'FlightSearch':
+                return <FlightsSearch/>;
+            default:
+                return <FlightsSearch/>;
+        }
+    };
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">
-                Your Logo
-            </a>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">
-                            Home
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <Registrate/>
-                    </li>
-                    <li className="nav-item">
-                        <Login/>
-                    </li>
-                    <li className="nav-item">
-                        {/* "Cabinet" button */}
-                        <button className="nav-link" onClick={handleCabinetClick}>
-                            Cabinet
-                        </button>
-                    </li>
-                    <li className="nav-item">
-                        <button className="nav-link" onClick={handleFlightSearchClick}>
-                            Flight Search
-                        </button>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="/">
+                    Your Logo
+                </a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/">
+                                Home
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <Registrate/>
+                        </li>
+                        <li className="nav-item">
+                            <Login/>
+                        </li>
+                        <li className="nav-item">
+                            {/* "Cabinet" button */}
+                            <button className="nav-link" onClick={handleCabinetClick}>
+                                Cabinet
+                            </button>
+                        </li>
+                        <li className="nav-item">
+                            <button className="nav-link" onClick={handleFlightSearchClick}>
+                                Flight Search
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            {renderActiveComponent()}
+
+        </div>
     );
 };
 
